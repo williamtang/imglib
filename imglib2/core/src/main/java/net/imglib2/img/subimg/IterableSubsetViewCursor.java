0089781b1672a4b14ed53156e63388cced82f201
Christian Dietz <christian.dietz@uni-konstanz.de>
2012-06-06 10:23:20 +0200

    Fixed another bug in IterableSubsetView

--- imglib2/core/src/main/java/net/imglib2/img/subimg/IterableSubsetViewCursor.java
+++ imglib2/core/src/main/java/net/imglib2/img/subimg/IterableSubsetViewCursor.java
@@ -7,55 +7,55 @@ import net.imglib2.type.NativeType;
 public class IterableSubsetViewCursor< T extends NativeType< T >> implements Cursor< T >
 {
 
-	private Cursor< T > m_fullCursor;
+	private Cursor< T > m_cursor;
 
 	private int m_typeIdx = 0;
 
-	private int m_offset;
+	private int m_planePos;
 
-	private int m_numElements;
+	private int m_planeSize;
 
 	private int m_numPlaneDims;
 
-	private T m_mainType;
+	private T m_type;
 
-	public IterableSubsetViewCursor( Cursor< T > cursor, int entries, int offset, int numPlaneDims )
+	public IterableSubsetViewCursor( Cursor< T > cursor, int planeSize, int planePos, int numPlaneDims )
 	{
-		m_fullCursor = cursor;
-		m_numElements = entries;
+		m_cursor = cursor;
+		m_planeSize = planeSize;
+		m_planePos = planePos;
 
 		m_numPlaneDims = numPlaneDims;
-		m_mainType = cursor.get();
 
-		m_offset = offset;
+		m_type = cursor.get();
+
 		reset();
-
 	}
 
 	@Override
 	public void localize( float[] position )
 	{
 		for ( int d = 0; d < m_numPlaneDims; d++ )
-			position[ d ] = m_fullCursor.getFloatPosition( d );
+			position[ d ] = m_cursor.getFloatPosition( d );
 	}
 
 	@Override
 	public void localize( double[] position )
 	{
 		for ( int d = 0; d < m_numPlaneDims; d++ )
-			position[ d ] = m_fullCursor.getDoublePosition( d );
+			position[ d ] = m_cursor.getDoublePosition( d );
 	}
 
 	@Override
 	public float getFloatPosition( int d )
 	{
-		return m_fullCursor.getFloatPosition( d );
+		return m_cursor.getFloatPosition( d );
 	}
 
 	@Override
 	public double getDoublePosition( int d )
 	{
-		return m_fullCursor.getDoublePosition( d );
+		return m_cursor.getDoublePosition( d );
 	}
 
 	@Override
@@ -67,50 +67,50 @@ public class IterableSubsetViewCursor< T extends NativeType< T >> implements Cur
 	@Override
 	public T get()
 	{
-		return m_mainType;
+		return m_type;
 	}
 
 	@Override
 	public Sampler< T > copy()
 	{
-		return m_fullCursor.copy();
+		return m_cursor.copy();
 	}
 
 	@Override
 	public void jumpFwd( long steps )
 	{
-		m_mainType.incIndex( ( int ) steps );
+		m_cursor.jumpFwd( ( int ) steps );
 		m_typeIdx += steps;
 	}
 
 	@Override
 	public void fwd()
 	{
-		m_mainType.incIndex();
+		m_cursor.fwd();
 		m_typeIdx++;
 	}
 
 	@Override
 	public void reset()
 	{
+		m_cursor.reset();
+		m_cursor.jumpFwd( m_planePos );
 		m_typeIdx = -1;
-		m_fullCursor.reset();
-		m_fullCursor.jumpFwd( m_offset );
 	}
 
 	@Override
 	public boolean hasNext()
 	{
-		return m_typeIdx < m_numElements - 1;
+		return m_typeIdx < m_planeSize - 1;
 	}
 
 	@Override
 	public T next()
 	{
-		m_mainType.incIndex();
+		m_cursor.fwd();
 		m_typeIdx++;
 
-		return m_mainType;
+		return m_type;
 	}
 
 	@Override
@@ -124,7 +124,7 @@ public class IterableSubsetViewCursor< T extends NativeType< T >> implements Cur
 	{
 		for ( int d = 0; d < m_numPlaneDims; d++ )
 		{
-			position[ d ] = m_fullCursor.getIntPosition( d );
+			position[ d ] = m_cursor.getIntPosition( d );
 		}
 	}
 
@@ -133,7 +133,7 @@ public class IterableSubsetViewCursor< T extends NativeType< T >> implements Cur
 	{
 		for ( int d = 0; d < m_numPlaneDims; d++ )
 		{
-			position[ d ] = m_fullCursor.getLongPosition( d );
+			position[ d ] = m_cursor.getLongPosition( d );
 		}
 
 	}
@@ -141,18 +141,18 @@ public class IterableSubsetViewCursor< T extends NativeType< T >> implements Cur
 	@Override
 	public int getIntPosition( int d )
 	{
-		return m_fullCursor.getIntPosition( d );
+		return m_cursor.getIntPosition( d );
 	}
 
 	@Override
 	public long getLongPosition( int d )
 	{
-		return m_fullCursor.getLongPosition( d );
+		return m_cursor.getLongPosition( d );
 	}
 
 	@Override
 	public Cursor< T > copyCursor()
 	{
-		return new IterableSubsetViewCursor< T >( m_fullCursor.copyCursor(), m_numElements, m_offset, m_numPlaneDims );
+		return new IterableSubsetViewCursor< T >( m_cursor.copyCursor(), m_planeSize, m_planePos, m_numPlaneDims );
 	}
 }
