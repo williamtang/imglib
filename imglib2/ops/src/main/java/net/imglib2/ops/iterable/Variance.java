336735d21df15200230b839e0900a18aee72ad6c
Christian Dietz <christian.dietz@uni-konstanz.de>
2012-06-27 12:40:56 +0200

    Fixed bug in variance calculation

--- imglib2/ops/src/main/java/net/imglib2/ops/iterable/Variance.java
+++ imglib2/ops/src/main/java/net/imglib2/ops/iterable/Variance.java
@@ -12,31 +12,34 @@ import net.imglib2.type.numeric.RealType;
  * @param <T>
  * @param <V>
  */
-public class Variance<T extends RealType<T>, V extends RealType<V>>
-		implements UnaryOperation<Iterable<T>, V> {
+public class Variance< T extends RealType< T >, V extends RealType< V >> implements UnaryOperation< Iterable< T >, V >
+{
 
 	@Override
-	public V compute(Iterable<T> input, V output) {
+	public V compute( Iterable< T > input, V output )
+	{
 		double sum = 0;
 		double sumSqr = 0;
 		int n = 0;
 
-		Iterator<T> iter = input.iterator();
+		Iterator< T > iter = input.iterator();
 
-		while (iter.hasNext()) {
+		while ( iter.hasNext() )
+		{
 			double px = iter.next().getRealDouble();
 			n++;
 			sum += px;
-			sumSqr = px * px;
+			sumSqr += px * px;
 		}
 
-		output.setReal((sumSqr - ((sum * sum) / n)) / (n - 1));
+		output.setReal( ( sumSqr - ( sum * sum / n ) ) / ( n - 1 ) );
 		return output;
 	}
 
 	@Override
-	public UnaryOperation<Iterable<T>, V> copy() {
-		return new Variance<T, V>();
+	public UnaryOperation< Iterable< T >, V > copy()
+	{
+		return new Variance< T, V >();
 	}
 
 }
