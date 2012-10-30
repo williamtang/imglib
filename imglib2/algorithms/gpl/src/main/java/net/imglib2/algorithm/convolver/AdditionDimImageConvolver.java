562f039768a5bf248e14c55fde9ed206023f542a
Christian Dietz <christian.dietz@uni-konstanz.de>
2012-10-30 13:10:17 +0100

    CleanUp of convolution

--- imglib2/algorithms/gpl/src/main/java/net/imglib2/algorithm/convolver/AdditionDimImageConvolver.java
+++ imglib2/algorithms/gpl/src/main/java/net/imglib2/algorithm/convolver/AdditionDimImageConvolver.java
@@ -14,54 +14,55 @@ import net.imglib2.type.numeric.RealType;
 /**
  * @author Christian Dietz (University of Konstanz)
  */
-public class AdditionDimImageConvolver<T extends RealType<T>, O extends RealType<O> & NativeType<O>, K extends RealType<K>>
-                implements MultiKernelConvolver<T, O, K> {
+public class AdditionDimImageConvolver< T extends RealType< T >, K extends RealType< K >, O extends RealType< O > & NativeType< O >> implements MultiKernelConvolver< T, K, O >
+{
 
-        private final Convolver<T, O, K> m_convolver;
+	private final Convolver< T, K, O > m_convolver;
 
-        public AdditionDimImageConvolver(Convolver<T, O, K> convolver) {
-                m_convolver = convolver;
-        }
+	public AdditionDimImageConvolver( Convolver< T, K, O > convolver )
+	{
+		m_convolver = convolver;
+	}
 
-        @Override
-        public RandomAccessibleInterval<O> compute(RandomAccessible<T> input,
-                        RandomAccessibleInterval<K>[] kernels,
-                        RandomAccessibleInterval<O> output) {
+	@Override
+	public RandomAccessibleInterval< O > compute( RandomAccessible< T > input, RandomAccessibleInterval< K >[] kernels, RandomAccessibleInterval< O > output )
+	{
 
-                final long[] min = new long[output.numDimensions()];
-                output.min(min);
-                final long[] max = new long[output.numDimensions()];
-                output.max(max);
-                for (int i = 0; i < kernels.length; i++) {
-                        max[max.length - 1] = i;
-                        min[min.length - 1] = i;
-                        m_convolver.compute(input, kernels[i], SubsetViews
-                                        .iterableSubsetView(output,
-                                                        new FinalInterval(min,
-                                                                        max)));
-                }
+		final long[] min = new long[ output.numDimensions() ];
+		output.min( min );
+		final long[] max = new long[ output.numDimensions() ];
+		output.max( max );
+		for ( int i = 0; i < kernels.length; i++ )
+		{
+			max[ max.length - 1 ] = i;
+			min[ min.length - 1 ] = i;
+			m_convolver.compute( input, kernels[ i ], SubsetViews.iterableSubsetView( output, new FinalInterval( min, max ) ) );
+		}
 
-                return output;
-        }
+		return output;
+	}
 
-        @Override
-        public AdditionDimImageConvolver<T, O, K> copy() {
-                return new AdditionDimImageConvolver<T, O, K>(
-                                (Convolver<T, O, K>) m_convolver.copy());
-        }
+	@Override
+	public AdditionDimImageConvolver< T, K, O > copy()
+	{
+		return new AdditionDimImageConvolver< T, K, O >( ( Convolver< T, K, O > ) m_convolver.copy() );
+	}
 
-        public static <T extends RealType<T>, O extends RealType<O> & NativeType<O>> Img<O> createEmptyOutput(
-                        Img<T> in, int numKernels, O resType) {
-                ImgFactory<O> imgFactory;
-                try {
-                        imgFactory = in.factory().imgFactory(resType);
-                } catch (IncompatibleTypeException e) {
-                        imgFactory = new ArrayImgFactory<O>();
-                }
-                final long[] dims = new long[in.numDimensions() + 1];
-                for (int d = 0; d < in.numDimensions(); d++)
-                        dims[d] = in.dimension(d);
-                dims[dims.length - 1] = numKernels;
-                return imgFactory.create(dims, resType);
-        }
+	public static < T extends RealType< T >, O extends RealType< O > & NativeType< O >> Img< O > createEmptyOutput( Img< T > in, int numKernels, O resType )
+	{
+		ImgFactory< O > imgFactory;
+		try
+		{
+			imgFactory = in.factory().imgFactory( resType );
+		}
+		catch ( IncompatibleTypeException e )
+		{
+			imgFactory = new ArrayImgFactory< O >();
+		}
+		final long[] dims = new long[ in.numDimensions() + 1 ];
+		for ( int d = 0; d < in.numDimensions(); d++ )
+			dims[ d ] = in.dimension( d );
+		dims[ dims.length - 1 ] = numKernels;
+		return imgFactory.create( dims, resType );
+	}
 }
--- imglib2/algorithms/gpl/src/main/java/net/imglib2/algorithm/convolver/ImgLib2FourierConvolver.java
+++ imglib2/algorithms/gpl/src/main/java/net/imglib2/algorithm/convolver/ImgLib2FourierConvolver.java
@@ -5,99 +5,101 @@ import net.imglib2.Interval;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.algorithm.fft2.FFTConvolution;
 import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.complex.ComplexFloatType;
 
 /**
  * Convolution, using ImgLib2Fourier implementation
- *
+ * 
  * @author Christian Dietz (University of Konstanz)
  */
-public class ImgLib2FourierConvolver<T extends RealType<T>, O extends RealType<O>, K extends RealType<K>>
-                implements Convolver<T, O, K> {
+public class ImgLib2FourierConvolver< T extends RealType< T >, K extends RealType< K >, O extends RealType< O >> implements Convolver< T, K, O >
+{
 
-        private RandomAccessible<T> m_lastImg = null;
+	private RandomAccessible< T > m_lastImg = null;
 
-        private KNIPFFTConvolution<T, O, K> m_fc = null;
+	private FFTConvolution< T, K, O > m_fc = null;
 
-        // Empty constructor for extension point
-        public ImgLib2FourierConvolver() {
-        }
+	// Empty constructor for extension point
+	public ImgLib2FourierConvolver()
+	{}
 
-        @Override
-        public ImgLib2FourierConvolver<T, O, K> copy() {
-                return new ImgLib2FourierConvolver<T, O, K>();
-        }
+	@Override
+	public ImgLib2FourierConvolver< T, K, O > copy()
+	{
+		return new ImgLib2FourierConvolver< T, O, K >();
+	}
 
-        @Override
-        public RandomAccessibleInterval<O> compute(
-                        final RandomAccessible<T> in,
-                        final RandomAccessibleInterval<K> kernel,
-                        final RandomAccessibleInterval<O> out) {
+	@Override
+	public RandomAccessibleInterval< O > compute( final RandomAccessible< T > in, final RandomAccessibleInterval< K > kernel, final RandomAccessibleInterval< O > out )
+	{
 
-                if (in.numDimensions() != kernel.numDimensions()) {
-                        throw new IllegalStateException(
-                                        "Kernel dimensions do not match to Img dimensions in ImgLibImageConvolver!");
-                }
+		if ( in.numDimensions() != kernel.numDimensions() ) { throw new IllegalStateException( "Kernel dimensions do not match to Img dimensions in ImgLibImageConvolver!" ); }
 
-                if (m_lastImg != in) {
-                        m_lastImg = new TmpWrapper<T>(in, out, kernel);
-                        m_fc = new KNIPFFTConvolution<T, O, K>(m_lastImg, out,
-                                        kernel, kernel, out,
-                                        new ArrayImgFactory<ComplexFloatType>());
-                        m_fc.setKernel(kernel);
-                        m_fc.setKeepImgFFT(true);
-                } else {
-                        m_fc.setKernel(kernel);
-                        m_fc.setOutput(out);
-                }
+		if ( m_lastImg != in )
+		{
+			m_lastImg = new TmpWrapper< T >( in, out, kernel );
+			m_fc = new KNIPFFTConvolution< T, O, K >( m_lastImg, out, kernel, kernel, out, new ArrayImgFactory< ComplexFloatType >() );
+			m_fc.setKernel( kernel );
+			m_fc.setKeepImgFFT( true );
+		}
+		else
+		{
+			m_fc.setKernel( kernel );
+			m_fc.setOutput( out );
+		}
 
-                m_fc.run();
+		m_fc.run();
 
-                return out;
-        }
+		return out;
+	}
 
-        // Workaround
-        private class TmpWrapper<TT extends RealType<TT>> implements
-                        RandomAccessible<TT> {
+	// Workaround
+	private class TmpWrapper< TT extends RealType< TT >> implements RandomAccessible< TT >
+	{
 
-                private final RandomAccessible<TT> m_accessible;
-                private final FinalInterval m_finalInterval;
+		private final RandomAccessible< TT > m_accessible;
 
-                public TmpWrapper(RandomAccessible<TT> accessible,
-                                Interval dims, Interval kernel) {
-                        // TODO: Workaround until fix in imglib2 (outofbounds
-                        // gets lost
-                        // during optimization of transformation)
-                        long[] min = new long[accessible.numDimensions()];
-                        long[] max = new long[accessible.numDimensions()];
+		private final FinalInterval m_finalInterval;
 
-                        for (int d = 0; d < kernel.numDimensions(); d++) {
-                                min[d] = -kernel.dimension(d);
-                                max[d] = kernel.dimension(d)
-                                                + dims.dimension(d);
-                        }
+		public TmpWrapper( RandomAccessible< TT > accessible, Interval dims, Interval kernel )
+		{
+			// TODO: Workaround until fix in imglib2 (outofbounds
+			// gets lost
+			// during optimization of transformation)
+			long[] min = new long[ accessible.numDimensions() ];
+			long[] max = new long[ accessible.numDimensions() ];
 
-                        m_accessible = accessible;
-                        m_finalInterval = new FinalInterval(min, max);
-                }
+			for ( int d = 0; d < kernel.numDimensions(); d++ )
+			{
+				min[ d ] = -kernel.dimension( d );
+				max[ d ] = kernel.dimension( d ) + dims.dimension( d );
+			}
 
-                @Override
-                public RandomAccess<TT> randomAccess() {
-                        return m_accessible.randomAccess(m_finalInterval);
-                }
+			m_accessible = accessible;
+			m_finalInterval = new FinalInterval( min, max );
+		}
 
-                @Override
-                public int numDimensions() {
-                        return m_accessible.numDimensions();
-                }
+		@Override
+		public RandomAccess< TT > randomAccess()
+		{
+			return m_accessible.randomAccess( m_finalInterval );
+		}
 
-                @Override
-                public RandomAccess<TT> randomAccess(Interval interval) {
-                        return m_accessible.randomAccess(interval);
-                };
+		@Override
+		public int numDimensions()
+		{
+			return m_accessible.numDimensions();
+		}
 
-        }
+		@Override
+		public RandomAccess< TT > randomAccess( Interval interval )
+		{
+			return m_accessible.randomAccess( interval );
+		};
+
+	}
 
 }
