d0ae1ab5cee66c7377799691ff552c0ebebd1199
Christian Dietz <christian.dietz@uni-konstanz.de>
2012-10-30 19:05:38 +0100

    Some changes in operation builder

--- imglib2/ops/src/main/java/net/imglib2/ops/img/OperationBuilder.java
+++ imglib2/ops/src/main/java/net/imglib2/ops/img/OperationBuilder.java
@@ -1,32 +1,19 @@
 package net.imglib2.ops.img;
 
 import net.imglib2.IterableInterval;
+import net.imglib2.ops.buffer.BufferFactory;
 import net.imglib2.ops.operation.BinaryOperation;
 import net.imglib2.ops.operation.UnaryOperation;
 
 public class OperationBuilder
 {
 
-	public static < A, B, C, D > UnaryOperation< A, D > concat( final UnaryOperation< A, B > op1, final UnaryOperation< A, C > op2, final BinaryOperation< B, C, D > binaryOp, final B bufferB, final C bufferC )
+	public static < A, B, C, D > UnaryOperation< A, D > concat( final BufferFactory< B > buf1, final BufferFactory< C > buf2, final UnaryOperation< A, B > op1, final UnaryOperation< A, C > op2, final BinaryOperation< B, C, D > binaryOp )
 	{
-		return new UnaryBinaryOperationAdapter< A, B, C, D >( op1, op2, binaryOp )
-		{
-
-			@Override
-			protected B getOp1Buffer()
-			{
-				return bufferB;
-			}
-
-			@Override
-			protected C getOp2Buffer()
-			{
-				return bufferC;
-			}
-		};
+		return new UnaryBinaryOperationAdapter< A, B, C, D >( op1, op2, binaryOp, buf1, buf2 );
 	}
 
-	public static < A, B, C > BinaryOperation< A, B[], C > concat( final C buffer, final BinaryOperation< A, B, C > binaryOp1, final BinaryOperation< C, B, C > binaryOp2 )
+	public static < A, B, C > BinaryOperation< A, B[], C > concat( final BufferFactory< C > buffer, final BinaryOperation< A, B, C > binaryOp1, final BinaryOperation< C, B, C > binaryOp2 )
 	{
 		return new BinaryOperation< A, B[], C >()
 		{
@@ -34,8 +21,9 @@ public class OperationBuilder
 			@Override
 			public C compute( A inputA, B[] inputB, C output )
 			{
-				binaryOp1.compute( inputA, inputB[ 0 ], buffer );
-				return binaryOp2.compute( buffer, inputB[ 1 ], output );
+				C buf = buffer.instantiate();
+				binaryOp1.compute( inputA, inputB[ 0 ], buf );
+				return binaryOp2.compute( buffer.instantiate(), inputB[ 1 ], output );
 			}
 
 			@Override
@@ -46,20 +34,12 @@ public class OperationBuilder
 		};
 	}
 
-	public static < A > UnaryOperation< A, A > concat( final A buffer, final UnaryOperation< A, A >... ops )
+	public static < A > UnaryOperation< A, A > concat( final BufferFactory< A > buff, final UnaryOperation< A, A >... ops )
 	{
-		return new ConcatenatedUnaryOperation< A >( ops )
-		{
-
-			@Override
-			protected A getBuffer()
-			{
-				return buffer;
-			}
-		};
+		return new ConcatenatedUnaryOperation< A >( buff, ops );
 	}
 
-	public static < A, B > UnaryOperation< A, B > concat( final B buffer, final UnaryOperation< A, B > first, final UnaryOperation< B, B >... second )
+	public static < A, B > UnaryOperation< A, B > concat( final BufferFactory< B > buffer, final UnaryOperation< A, B > first, final UnaryOperation< B, B >... second )
 	{
 
 		if ( second.length % 2 == 1 )
@@ -87,30 +67,28 @@ public class OperationBuilder
 		}
 	}
 
-	public static < A, B > UnaryOperation< A, B > concat( final B buffer, final UnaryOperation< A, B > first, final UnaryOperation< B, B > second )
+	public static < A, B, C > UnaryOperation< A, C > concat( final BufferFactory< B > buffer, final UnaryOperation< A, B > first, final UnaryOperation< B, C > second )
 	{
-		return new UnaryOperationBridge< A, B, B >( first, second )
-		{
 
-			@Override
-			public B getBuffer()
-			{
-				return buffer;
-			}
-		};
+		// SingletonBufferFactory< B > singletonFactory = new
+		// SingletonBufferFactory< B >( buffer );
+		//
+		// if ( first instanceof BufferedOperation && ( ( BufferedOperation< B >
+		// ) first ).bufferFactory().equals( buffer ) )
+		// ( ( BufferedOperation< B > ) second ).setBufferFactory(
+		// singletonFactory );
+		//
+		// if ( second instanceof BufferedOperation && ( ( BufferedOperation< B
+		// > ) first ).bufferFactory().equals( buffer ) )
+		// ( ( BufferedOperation< B > ) second ).setBufferFactory(
+		// singletonFactory );
+
+		return new UnaryOperationBridge< A, B, C >( buffer, first, second );
 	}
 
-	public static < A, B, C, D > BinaryOperation< A, B, D > concat( final C buffer, BinaryOperation< A, B, C > binaryOp, UnaryOperation< C, D > unaryOp )
+	public static < A, B, C, D > BinaryOperation< A, B, D > concat( final BufferFactory< C > buffer, BinaryOperation< A, B, C > binaryOp, UnaryOperation< C, D > unaryOp )
 	{
-		return new BinaryUnaryOperationAdapter< A, B, C, D >( binaryOp, unaryOp )
-		{
-
-			@Override
-			protected C getBinaryOpBuffer()
-			{
-				return buffer;
-			}
-		};
+		return new BinaryUnaryOperationAdapter< A, B, C, D >( buffer, binaryOp, unaryOp );
 	}
 
 	public static < A, B > UnaryOperation< IterableInterval< A >, IterableInterval< B >> map( UnaryOperation< A, B > op )
