f304c5968cd9afff574598a2f183d4f95d0ed358
Martin Horn <martin.horn@uni-konstanz.de>
2012-11-29 09:55:52 +0100

    Removed copyright header for MemoryWarningSystem.java

--- imglib2/core/src/main/java/net/imglib2/img/file/MemoryWarningSystem.java
+++ imglib2/core/src/main/java/net/imglib2/img/file/MemoryWarningSystem.java
@@ -1,25 +1,3 @@
-/* ------------------------------------------------------------------
- * This source code, its documentation and all appendant files
- * are protected by copyright law. All rights reserved.
- *
- * Copyright, 2008 - 2012
- * KNIME.com, Zurich, Switzerland
- *
- * You may not modify, publish, transmit, transfer or sell, reproduce,
- * create derivative works from, distribute, perform, display, or in
- * any way exploit any of the content, in whole or in part, except as
- * otherwise expressly permitted in writing by the copyright owner or
- * as specified in the license file distributed with this product.
- *
- * If you have any questions please contact the copyright holder:
- * website: www.knime.com
- * email: contact@knime.com
- * ---------------------------------------------------------------------
- *
- * History
- *   Sep 6, 2012 (hornm): created
- */
-
 package net.imglib2.img.file;
 
 import java.lang.management.ManagementFactory;
@@ -39,6 +17,8 @@ import javax.management.NotificationListener;
  * percentage of available memory specified. There should only be one instance
  * of this object created, since the usage threshold can only be set to one
  * number.
+ * 
+ * source: www.javaspecialists.eu/archive/Issue092.html
  */
 public class MemoryWarningSystem
 {
