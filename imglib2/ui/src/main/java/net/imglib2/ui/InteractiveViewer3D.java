858dd5a65b65a406bbf4c5de7deaf753242657a4
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2013-04-19 17:09:11 +0200

    added NLinearInterpolatorARGB and factory that internally uses doubles to accumulate channels and later clips the result to [0--255] (cherry picked from commit 75dd8b10088478c1c671fcbc810123f78d7dd1ed)

--- imglib2/ui/src/main/java/net/imglib2/ui/InteractiveViewer3D.java
+++ imglib2/ui/src/main/java/net/imglib2/ui/InteractiveViewer3D.java
@@ -46,6 +46,7 @@ import net.imglib2.converter.Converter;
 import net.imglib2.display.XYRandomAccessibleProjector;
 import net.imglib2.interpolation.Interpolant;
 import net.imglib2.interpolation.InterpolatorFactory;
+import net.imglib2.interpolation.randomaccess.NLinearInterpolatorARGBFactory;
 import net.imglib2.interpolation.randomaccess.NLinearInterpolatorFactory;
 import net.imglib2.interpolation.randomaccess.NearestNeighborInterpolatorFactory;
 import net.imglib2.realtransform.AffineGet;
@@ -123,6 +124,7 @@ public class InteractiveViewer3D< T extends NumericType< T > > extends AbstractI
 		display.requestRepaint();
 	}
 
+	@SuppressWarnings( { "unchecked", "rawtypes" } )
 	@Override
 	protected XYRandomAccessibleProjector< T, ARGBType > createProjector()
 	{
@@ -134,7 +136,10 @@ public class InteractiveViewer3D< T extends NumericType< T > > extends AbstractI
 			break;
 		case 1:
 		default:
-			interpolatorFactory = new NLinearInterpolatorFactory< T >();
+			if ( ARGBType.class.isInstance( source.randomAccess().get() ) )
+				interpolatorFactory = ( InterpolatorFactory )new NLinearInterpolatorARGBFactory();
+			else
+				interpolatorFactory = new NLinearInterpolatorFactory< T >();
 			break;
 		}
 		final Interpolant< T, RandomAccessible< T > > interpolant = new Interpolant< T, RandomAccessible< T > >( source, interpolatorFactory );
