3dc4a8e5a8678b63721d414b1c010ba73df723a5
Curtis Rueden <ctrueden@wisc.edu>
2013-05-01 13:47:08 -0500

    Utils: Use TypedUnitSpace in getSpatialCalibration
    This is in preparation for the retirement of CalibratedSpace.

--- algorithms/gpl/src/main/java/net/imglib2/algorithm/region/localneighborhood/Utils.java
+++ algorithms/gpl/src/main/java/net/imglib2/algorithm/region/localneighborhood/Utils.java
@@ -27,7 +27,8 @@
 package net.imglib2.algorithm.region.localneighborhood;
 
 import net.imglib2.meta.Axes;
-import net.imglib2.meta.Metadata;
+import net.imglib2.meta.TypedUnitAxis;
+import net.imglib2.meta.TypedUnitSpace;
 import net.imglib2.util.Util;
 
 /**
@@ -38,19 +39,20 @@ public class Utils {
 	
 	
 	/**
-	 * Return the xyz calibration stored in an {@link Metadata} in a 3-elements
+	 * Return the xyz calibration stored in a {@link TypedUnitSpace} in a 3-elements
 	 * double array. Calibration is ordered as X, Y, Z. If one axis is not found,
 	 * then the calibration for this axis takes the value of 1.
 	 */
-	public static final double[] getSpatialCalibration(final Metadata img) {
+	public static final double[] getSpatialCalibration(final TypedUnitSpace<?> space) {
 		final double[] calibration = Util.getArrayFromValue(1d, 3);
-		for (int d = 0; d < img.numDimensions(); d++) {
-			if (img.axis(d).equals(Axes.X)) {
-				calibration[0] = img.calibration(d);
-			} else if (img.axis(d).equals(Axes.Y)) {
-				calibration[1] = img.calibration(d);
-			} else if (img.axis(d).equals(Axes.Z)) {
-				calibration[2] = img.calibration(d);
+		for (int d = 0; d < space.numDimensions(); d++) {
+			final TypedUnitAxis axis = space.axis(d);
+			if (axis.type() == Axes.X) {
+				calibration[0] = axis.calibration();
+			} else if (axis.type() == Axes.Y) {
+				calibration[1] = axis.calibration();
+			} else if (axis.type() == Axes.Z) {
+				calibration[2] = axis.calibration();
 			}
 		}
 		return calibration;
